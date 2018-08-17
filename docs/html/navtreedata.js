f6198cd7b77fe88c5b0d64cc07b61204f1bfae0e
frainfreeze <kucar.tomislav@gmail.com>
2018-08-18 00:10:13 +0200

    update docs

--- docs/html/navtreedata.js
+++ docs/html/navtreedata.js
@@ -6,7 +6,8 @@ var NAVTREE =
       [ "File List", "files.html", "files" ],
       [ "File Members", "globals.html", [
         [ "All", "globals.html", null ],
-        [ "Functions", "globals_func.html", null ]
+        [ "Functions", "globals_func.html", null ],
+        [ "Variables", "globals_vars.html", null ]
       ] ]
     ] ]
   ] ]
